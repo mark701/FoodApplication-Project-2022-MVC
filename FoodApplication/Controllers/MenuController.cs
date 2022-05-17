971518435625573a5052ea3ecc6b1e43df5d50ef
mark emad <mark.emad83@gmail.com>
2022-05-17 09:21:23 +0200

    add  menu controller and its view

---
+++ FoodApplication/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿
+using FoodApplication.Database;
+using FoodApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodApplication.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        private readonly FoodDataBaseEntities foodDB;
+        dynamic mymodel = new ExpandoObject();
+        public MenuController()
+        {
+            foodDB = new FoodDataBaseEntities();
+        }
+        public ActionResult MenuFood()
+        {
+
+
+
+            List<Product> allFood = foodDB.Products.ToList();
+            /*var Sproduct = database.Products.Where(name => name.P_ID == "0xC1" && name.P_price==12);*/
+            /*var Sproduct2 = database.Products.Find("0xc1");*/
+            //mymodel.Product = allFood;
+
+
+            /*var allfoodModel = mapper.Map<List <Food>>(allFood);*/
+
+            ViewBag.MenuFood = allFood;
+            ViewBag.UserData = DataUser.user;
+            return View(ViewBag);
+        }
+    }
+}
